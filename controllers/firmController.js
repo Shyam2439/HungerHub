6136350b7d6b8ebb5bfb7eb1ae261eddfdfb0ce3
Shyam2439 <168397511+Shyam2439@users.noreply.github.com>
2025-05-14 11:21:42 +0530

    Add files via upload

---
+++ controllers/firmController.js
@@ -0,0 +1,60 @@
+const Firm = require('../models/Firm');
+const Vendor = require('../models/vendor');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() +path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage: storage });
+
+const addFirm = async (req, res) => {
+    try {
+        const { firmName, area, category, region, offer } = req.body;
+        const image = req.file ? req.file.filename : undefined;
+
+        const vendor = await Vendor.findById(req.vendorId);
+        if (!vendor) {
+            return res.status(404).json({ message: "Vendor not found" });
+        }
+
+        const firm = new Firm({
+            firmName,
+            area,
+            category,
+            region,
+            offer,
+            image,
+            vendor: vendor._id
+        });
+
+        const savedFirm = await firm.save();
+
+        vendor.firm.push(savedFirm._id);
+        await vendor.save();
+
+        return res.status(200).json({ message: "Firm added successfully", firm: savedFirm });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+const deleteFirmById = async(req,res)=>{
+    try {
+        const firmId = req.params.firmId;
+        const deleteProduct = await Firm.findByIdAndDelete(firmId);
+        if(!deleteProduct){
+            return res.status(404).json({error:"no product found"});
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error:"internal server error"});
+    }
+}
+module.exports = {
+    addFirm: [upload.single('image'), addFirm,deleteFirmById]
+};
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const dotEnv = require("dotenv");
+const mongoose = require("mongoose");
+const vendorRoutes = require('./routes/vendorRoutes');
+const productRoutes = require('./routes/productRoutes');
+const bodyparser = require('body-parser');
+const firmRoutes = require('./routes/firmRoutes');
+const path = require('path');
+
+const app = express()
+const PORT = 4000;
+
+dotEnv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=> console.log("MongoDb connected"))
+.catch((error)=>console.log(error))
+
+    app.use(bodyparser.json());
+    app.use('/vendor',vendorRoutes);
+    app.use('/firm',firmRoutes);
+    app.use('/product',productRoutes);
+    app.use('/uploads',express.static('uploads'))
+app.listen(PORT,()=>{
+    console.log(`server is running at port ${PORT}`)
+})
+
+app.use('/home',(req,res)=>{
+    res.send("<h1> Welcome to Project");
+})
